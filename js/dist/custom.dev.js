faa0e61d57bddf5afc42dac30646f4177272f64d
nermenalrayyan <n.alrayyan.work@gmail.com>
2022-08-09 01:41:16 +0400

    delete padding bottom on contact page

--- js/dist/custom.dev.js
+++ js/dist/custom.dev.js
@@ -1,13 +1,10 @@
 "use strict";
 
 function toggleMenu() {
-  var toggleMenu = document.querySelector('.toggleMenu'); // const menu = document.querySelector('.menu');
-
+  var toggleMenu = document.querySelector('.toggleMenu');
   var navLinks = document.querySelector('.navLinks');
-  var btns = document.querySelector('.btns'); // const toggleMenu = document.querySelector('.toggleMenu');
-
-  toggleMenu.classList.toggle("active"); // menu.classList.toggle("active");
-
+  var btns = document.querySelector('.btns');
+  toggleMenu.classList.toggle("active");
   navLinks.classList.toggle("active");
   btns.classList.toggle("active");
 }
